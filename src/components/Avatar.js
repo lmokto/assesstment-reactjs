ba627eb403efcb4fb4936a49e4e328558e24f6fd
Luis Uranga <luisuranga.urangaruiz@altran.com>
2019-07-16 11:10:24 +0200

    list professions and friends

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,6 +3,9 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import { List, ListSubheader } from '@material-ui/core';
 
 const Avatar = (elems) => {
     return(
@@ -17,12 +20,31 @@ const Avatar = (elems) => {
                         <Typography gutterBottom variant="headline" component="h2">
                             {elems.population.name}
                         </Typography>
-                        <Typography component="p">
-                            {elems.population.hair_color}
+                        <Typography component="h3">
+                            Friends
                         </Typography>
-                        <Typography component="p">
-                            {elems.population.age} - {elems.population.height} - {elems.population.weight}
-                        </Typography>                        
+                        <ListItem>
+                            { elems.population.friends.map(name => (
+                                <ListItemText>{name}</ListItemText>
+                            ))}
+                        </ListItem>
+                        <Typography component="h3">
+                            Professions
+                        </Typography>              
+                        <ListItem>
+                            { elems.population.professions.map(skill => (
+                                <ListItemText>{skill}</ListItemText>
+                            ))}
+                        </ListItem>
+                        <Typography component="h3">
+                            Look
+                        </Typography>
+                        <ListItem>
+                            <ListItemText>Age {elems.population.age}</ListItemText>
+                            <ListItemText>Height {elems.population.height}</ListItemText>
+                            <ListItemText>Weight {elems.population.weight}</ListItemText>
+                            <ListItemText>Hair {elems.population.hair_color}</ListItemText>
+                        </ListItem>
                     </CardContent>
                 </Card>
           ): null }  
